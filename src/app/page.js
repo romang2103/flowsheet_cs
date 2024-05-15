50aeca6be3f41af9354fdfe309f9590ff3d7890a
romang2103 <romang2103@gmail.com>
2024-05-15 22:32:15 +0100

    progress_1

--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,27 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [modules, setModules] = useState([]);
+  const [yearRowSpan, setYearRowSpan] = useState(2);
+  const [semesterRowSpan, setSemesterRowSpan] = useState(1);
+
+  // Fetch data from the API
+  useEffect(() => {
+    async function fetchData() {
+      console.log("fetching data...");
+      const response = await fetchData("api/modules.js");
+      const data = await response.json();
+      setModules(data);
+      console.log(modules);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 border-solid border-2 border-indigo-600">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex border-solid border-2 border-indigo-600">
-        <div class="flex flex-col">
-          <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-              <div class="overflow-hidden">
-                <table class="min-w-full text-left text-sm font-light text-surface dark:text-white">
-                  <thead class="border-b border-neutral-200 font-medium dark:border-white/10">
-                    <tr>
-                      <th scope="col" class="px-6 py-4">
-                        #
-                      </th>
-                      <th scope="col" class="px-6 py-4">
-                        First
-                      </th>
-                      <th scope="col" class="px-6 py-4">
-                        Last
-                      </th>
-                      <th scope="col" class="px-6 py-4">
-                        Handle
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr class="border-b border-neutral-200 transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-white/10 dark:hover:bg-neutral-600">
-                      <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                      <td class="whitespace-nowrap px-6 py-4">Mark</td>
-                      <td class="whitespace-nowrap px-6 py-4">Otto</td>
-                      <td class="whitespace-nowrap px-6 py-4">@mdo</td>
-                    </tr>
-                    <tr class="border-b border-neutral-200 transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-white/10 dark:hover:bg-neutral-600">
-                      <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                      <td class="whitespace-nowrap px-6 py-4">Jacob</td>
-                      <td class="whitespace-nowrap px-6 py-4">Thornton</td>
-                      <td class="whitespace-nowrap px-6 py-4">@fat</td>
-                    </tr>
-                    <tr class="border-b border-neutral-200 transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-white/10 dark:hover:bg-neutral-600">
-                      <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
-                      <td class="whitespace-nowrap px-6 py-4">Larry</td>
-                      <td class="whitespace-nowrap px-6 py-4">Wild</td>
-                      <td class="whitespace-nowrap px-6 py-4">@twitter</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+    <div>
+      <p>Modules: {modules}</p>
+    </div>
   );
 }
